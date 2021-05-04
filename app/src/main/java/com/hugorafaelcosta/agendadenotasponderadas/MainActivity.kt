c5c4af0d2789d9aba85584e49d58020a10c6a13a
Hugo Costa <costarafaelhugo@gmail.com>
2021-05-04 09:09:57 -0300

    Adicionei a funcionalidade que não permite serem inseridos numeros <=0 nos pesoas da notas e solicita um numero válido caso isso ocorra.

--- app/src/main/java/com/hugorafaelcosta/agendadenotasponderadas/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/agendadenotasponderadas/MainActivity.kt
@@ -16,22 +16,26 @@ class MainActivity : AppCompatActivity() {
             val num2 = binding.nota2
             val peso1 = binding.PesoNota1
             val peso2 = binding.PesoNota2
+            val resultado = binding.resultado
+
 
             val nota1 = num1.text.toString().toInt()
             val nota2 = num2.text.toString().toInt()
             val pesoN1 = peso1.text.toString().toInt()
             val pesoN2 = peso2.text.toString().toInt()
 
-            calculaMedia(nota1, nota2, pesoN1, pesoN2)
+
+            if (pesoN1 <= 0 || pesoN2 <= 0) {
+                resultado.setText("Digite um numero válido ")
+            } else {
+                calculaMedia(nota1, nota2, pesoN1, pesoN2)
+            }
         }
     }
-
     fun calculaMedia(nota1: Int, nota2: Int, pesoN1: Int, pesoN2: Int) {
         val media = (nota1 * pesoN1 + nota2 * pesoN2) / (pesoN1 + pesoN2)
         val resultado = binding.resultado
-//        if(pesoN1 <= 1 || pesoN2 <= 1){
-//         resultado.text = "Digite o peso válido"
-//        }
+//
         if (media >= 5) {
             resultado.text = "A nota final é $media \n Aluno Aprovado"
             resultado.setTextColor(getColor(R.color.green))
